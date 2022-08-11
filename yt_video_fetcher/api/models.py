71c5cbf7022cd974f4926165250e155a56de19ba
flobiz-anay <anay.joshi@flobiz.in>
2022-08-11 22:33:00 +0530

    Added Video model and some setting changes

--- yt_video_fetcher/api/models.py
+++ yt_video_fetcher/api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+# Video Model
+class Video(models.Model):
+  video_id = models.CharField(max_length=250, unique=True)
+  video_title = models.CharField(max_length=250)
+  video_thumbnail = models.URLField()
+  channel_id = models.CharField(max_length=250)
+  channel_title = models.CharField(max_length=250)
+  description = models.CharField(max_length=1250)
+  uploaded_at = models.DateTimeField()
+  
+  def __str__(self):
+    return self.video_title
